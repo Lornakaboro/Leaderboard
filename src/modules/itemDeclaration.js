2eacff57f133ac40ea5504183d3aecb320ad97c9
Lornakaboro <lornakaboro@gmail.com>
2023-03-10 14:59:09 +0300

    Add DOM items module

---
+++ src/modules/itemDeclaration.js
@@ -0,0 +1,8 @@
+const displaySection = document.querySelector('.score-list');
+const user = document.getElementById('name');
+const score = document.getElementById('score');
+const addBtn = document.getElementById('btn');
+
+export {
+  displaySection, user, score, addBtn,
+};
